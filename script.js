2648200cb0ea24a07c709ce73fb0e012be980c4d
Alon Ribeiro <56462713+AlonRibeiro12@users.noreply.github.com>
2021-06-16 13:17:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+    function calcular() {
+        let entrada = parseInt(document.getElementById('entrada').value);
+        for(i=1; i<=100; i++)
+        console.log(entrada+"X" + i + "=" + (entrada*i));
+    }
